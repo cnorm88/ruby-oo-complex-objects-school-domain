015996502bc986855f768856420c32eeab8c3bc7
cnorm88 <cnorm88@gmail.com>
2021-02-20 18:09:19 -0500

    commit tree 013e9d2f29e95c89cc75645c0f84e9fa13f0b335

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+class School
+
+attr_reader :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student, grade)
+        roster[grade] ||= []
+        roster[grade] << student
+    end
+
+    def grade(students)
+        roster[students]
+    end
+
+    def sort
+    roster.each do |grade, name|
+    roster[grade] = name.sort
+    end
+    end
+
+end
